c84ce1af62231a41f2d938b4b8ed9d5e24673e8b
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-12-21 22:11:42 -0800

    Update Main.java

--- Interfaces/Example/Main.java
+++ Interfaces/Example/Main.java
@@ -1,1 +1,15 @@
+public class Main {
+    public static void main(String[] arg) {
+        print(Example.name); // I can call the field because of the static keyword
 
+        Example examples = new Test();
+        examples.printPrice(Example.settingPrice);
+        examples.getName();
+
+        examples.writeExample();
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
