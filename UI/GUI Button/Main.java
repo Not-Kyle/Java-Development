b0eb59d069e363bc967a4d84c1c40f9c30a5d99e
kori <77679412+Not-Kyle@users.noreply.github.com>
2025-12-22 11:10:30 -0800

    Create Main.java

---
+++ UI/GUI Button/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] arg) {
+        Frame frame = new Frame();
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
+    }
+}
